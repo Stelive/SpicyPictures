553352aee5b31647c8fb3e6714ad70dec9448604
HiGringo <panettafrancesco8@gmail.com>
2018-05-20 19:07:22 +0200

    Modifiche di Domenica

--- github/ProgettoFinale/js/main.js
+++ github/ProgettoFinale/js/main.js
@@ -5,3 +5,25 @@
  * Purpose of file:
  * ...
  */
+
+$(window).load(function(){
+    setTimeout(function(){
+        $('div.loading').velocity({
+            opacity : 0.1,
+            translateY: "-80px"
+        }, {
+            duration: 400,
+            complete: function(){
+            $('div.sfondo').velocity({
+            translateY : "-100%"
+        }, {
+            duration: 1000,
+            easing: [0.7,0,0.3,1],
+            complete: function(){
+                $('.home').addClass('animate-border divide');
+            }
+        })  
+            }
+        })
+    },1000)
+})
