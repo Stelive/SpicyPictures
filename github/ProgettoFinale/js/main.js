99759b6cdcbb33eabde7977b4d3dbe0eed2b18a4
Stefano <stefano.pedroli@fastartconsulting.com>
2018-05-21 15:35:28 +0200

    Aggiunta di jquery alle pagine e isolamento animazione contact us

--- github/ProgettoFinale/js/main.js
+++ github/ProgettoFinale/js/main.js
@@ -6,24 +6,7 @@
  * ...
  */
 
-$(window).load(function(){
-    setTimeout(function(){
-        $('div.loading').velocity({
-            opacity : 0.1,
-            translateY: "-80px"
-        }, {
-            duration: 400,
-            complete: function(){
-            $('div.sfondo').velocity({
-            translateY : "-100%"
-        }, {
-            duration: 1000,
-            easing: [0.7,0,0.3,1],
-            complete: function(){
-                $('.home').addClass('animate-border divide');
-            }
-        })  
-            }
-        })
-    },500)
-})
+$('.googleMap').ready(function() {
+    // Handler for .load() called.
+    
+});
