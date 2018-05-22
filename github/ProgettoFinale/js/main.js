09dd51880a673d2529508f77ea060c1b6fff0794
HiGringo <panettafrancesco8@gmail.com>
2018-05-22 18:51:22 +0200

    modifiche mie responsive

--- github/ProgettoFinale/js/main.js
+++ github/ProgettoFinale/js/main.js
@@ -5,7 +5,14 @@
  * Purpose of file:
  * ...
  */
-
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
  
 $('.googleMap').ready(function() {
     // Handler for .load() called.
